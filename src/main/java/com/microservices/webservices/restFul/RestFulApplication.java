8f761b121c5b0026a154a4b973c1580deacd0093
unknown <abdul_abid@outlook.com>
2022-01-14 08:29:12 +0530

    rest apiss

---
+++ src/main/java/com/microservices/webservices/restFul/RestFulApplication.java
@@ -0,0 +1,12 @@
+package com.microservices.webservices.restFul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFulApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestFulApplication.class, args);
+	}
+
+}
